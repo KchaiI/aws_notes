d3e59d5b567b9efd6bd5cad933bf4194cfdc0179
kaichi0michael <ihciak.0127@gmail.com>
2026-05-12 10:00:39 +0900

    backend.hcl

--- case04/infra/envs/dev/backend.tf
+++ case04/infra/envs/dev/backend.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    bucket         = "task-api-tfstate-058898200941"
     key            = "envs/dev/terraform.tfstate"
     region         = "ap-northeast-1"
     dynamodb_table = "task-api-tflock"
