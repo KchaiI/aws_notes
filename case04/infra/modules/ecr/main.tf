b8d54dd7acc1f300168e1fab2d3767b076bd8de1
kaichi0michael <ihciak.0127@gmail.com>
2026-05-17 04:29:53 +0900

    modify

--- case04/infra/modules/ecr/main.tf
+++ case04/infra/modules/ecr/main.tf
@@ -8,6 +8,7 @@ locals {
 resource "aws_ecr_repository" "this" {
   name                 = "${local.name_prefix}-${var.repository_name}"
   image_tag_mutability = "MUTABLE"  # 同じタグの上書きを許可（latestタグなどで便利）
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true  # push時に脆弱性スキャンを自動実行
